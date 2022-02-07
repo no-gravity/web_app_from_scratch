9c6341bd44e5f4700a49ed58d6b188713bcae0bd
Meet Gor <40317114+Mr-Destructive@users.noreply.github.com>
2022-02-07 17:03:51 +0530

    added user auth in django script
    Added Authorization using Django User Creation Forms in the Django setup script.

--- django.sh
+++ django.sh
@@ -42,7 +42,6 @@ apt install -y apache2
 apt install -y libapache2-mod-wsgi-py3
 
 cat << 'EOF' > /etc/apache2/sites-enabled/000-default.conf
-ServerName mysite.local
 WSGIPythonPath /var/www/mysite
 <VirtualHost *:80>
     WSGIScriptAlias / /var/www/mysite/mysite/wsgi.py
@@ -56,7 +55,10 @@ EOF
 
 service apache2 start
 
-read -p 'Django is live at 127.0.0.1! Hit enter to continue.'
+# Yay, we have a working Django instance!
+# Now 127.0.0.1 has the user page
+# And 127.0.0.1/admin/ has an admin page
+read -p 'Django is running! Hit enter to continue.'
 
 # ===================
 # Let's use templates
@@ -66,15 +68,19 @@ read -p 'Django is live at 127.0.0.1! Hit enter to continue.'
 cd mysite
 
 mkdir templates
+mkdir templates/mysite
 
-cat << 'EOF' > templates/index.html
+touch templates/mysite/index.html
+touch templates/mysite/base.html
+
+cat << 'EOF' > templates/mysite/index.html
 <h1>Hello World</h1>
 EOF
 
 cat << 'EOF' > views.py
 from django.shortcuts import render
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'mysite/index.html')
 EOF
 
 cat << 'EOF' > urls.py
@@ -84,7 +90,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='index'),
 ]
 EOF
 
@@ -102,7 +108,7 @@ read -p 'Serving a static site! Hit enter to continue.'
 # =========================
 
 # Let's create a base template 
-cat << 'EOF' > templates/base.html
+cat << 'EOF' > templates/mysite/base.html
 <!DOCTYPE html>
 <html>
 <head>
@@ -116,8 +122,8 @@ cat << 'EOF' > templates/base.html
 EOF
 
 # And use it for the index page:
-cat << 'EOF' > templates/index.html
-{% extends "base.html" %}
+cat << 'EOF' > templates/mysite/index.html
+{% extends "mysite/base.html" %}
 {% block content %}
     <h1>Hello World</h1>
 {% endblock %}
@@ -131,5 +137,167 @@ read -p 'The base template is live! Hit enter to continue.'
 # =======================
 # Let's add user accounts
 # =======================
+cd ..
 
-# ... To be written ...
+# Create a new app called user for Auth
+django-admin startapp user
+
+cat << 'EOF' >> mysite/settings.py
+INSTALLED_APPS += ['user']
+EOF
+
+mkdir user/templates/
+mkdir user/templates/user
+touch user/forms.py
+
+# Apply Auth model migrations to db
+python manage.py makemigrations
+python manage.py migrate
+
+# Create a User Registration Form 
+# Extends the UserCreationForm
+cat << 'EOF' > user/forms.py
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model= User
+        fields = ['username', 'email', 'password1', 'password2']
+EOF
+
+# Create some basic views for registration
+cat << 'EOF' > user/views.py
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+
+def index(request):
+    return render(request, 'user/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'user/register.html', {'form': form})
+
+EOF
+
+# Create the URL mappings of Auth
+cat << 'EOF' > user/urls.py
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from user import views as user_views
+
+urlpatterns = [
+    path('', user_views.index, name='auth'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+]
+EOF
+
+cat << 'EOF' > mysite/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('auth/', include('user.urls')),
+]
+EOF
+
+# Create Templates for Authorization
+touch user/templates/user/index.html
+touch user/templates/user/register.html
+touch user/templates/user/login.html
+touch user/templates/user/logout.html
+
+cat << 'EOF' > user/templates/user/index.html
+{% extends 'mysite/base.html' %}
+
+{% block content %}
+    <a href="{% url 'register' %}"><button>Register</button></a>
+    <a href="{% url 'login' %}"><button>Login</button></a>
+{% endblock %}
+
+EOF
+
+cat << 'EOF' > user/templates/user/register.html
+{% extends 'mysite/base.html' %}
+
+{% block content %}
+    <form method="POST">
+        {% csrf_token %}
+            {{ form.as_p }}
+            <button type="submit">Sign Up</button>
+    </form>
+        <p>
+        Already have an Account? <a href="{% url 'login' %}"><button>Log In</button></a>
+        </p>
+{% endblock %}
+
+
+EOF
+
+cat << 'EOF' > user/templates/user/login.html
+{% extends 'mysite/base.html' %}
+
+{% block  content %}
+    <form method="POST">
+        {% csrf_token %}
+            {{ form.as_p }}
+            <button type="submit">Log In</button>
+    </form>
+        <p>
+            Don\'t have an Account? <a href="{% url 'register' %}"><button>Register</button></a>
+        </p>
+{% endblock %}
+EOF
+
+cat << 'EOF' > user/templates/user/logout.html
+{% extends 'mysite/base.html' %}
+
+{% block content %}
+        <p>
+            Don\'t have an account?
+            <a href="{% url 'register' %}"><button>Register</button></a>
+        </p>
+        <p>
+            Already have an Account?
+            <a href="{% url 'login' %}"><button>Login</button></a>
+        </p>
+{% endblock %}
+
+EOF
+
+# Create a Login redirect URL route
+cat << 'EOF' >> mysite/settings.py
+LOGIN_REDIRECT_URL = 'index'
+EOF
+
+sed -i "s/^M//" mysite/settings.py
+
+cat << 'EOF' > mysite/templates/mysite/index.html
+{% extends "mysite/base.html" %}
+{% block content %}
+    {% if user.is_authenticated %}
+    <h1>Hello, {{ user.username }}!</h1>
+    <h3>You are logged in as {{ user.username }} </h3>
+    <a href="{% url 'logout' %}"><button>Logout</button></a>
+    {% else %}
+    <h1>Hello, World!</h1>
+    <a href="{% url 'auth' %}"><button>Authorize</button></a>
+    {% endif %}
+{% endblock %}
+
+EOF
