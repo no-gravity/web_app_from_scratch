069814b089329d6404395b49611e4726938a51da
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2022-02-26 12:59:59 +0530

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -3,7 +3,8 @@
 # ======================
 
 # This page contains a list of terminal commands that
-# create a Flask web application with  templates
+# create a Flask web application with routing, templates
+# and user accounts.
 #
 # You can start from a fresh debian installation. Or with a
 # fresh debian container:
@@ -29,18 +30,17 @@ apt update -y && apt upgrade -y
 
 cd /var/www
 apt install -y python3-flask
-mkdir -p mysite
+mkdir mysite
 cd mysite
 
-mkdir -p flasky
-cd flasky
-
-mkdir -p logs
-mkdir -p static
+mkdir mysite
+mkdir logs
+mkdir static
 
 # ====================
 # Let's install Apache
 # ====================
+
 apt install -y apache2
 apt install -y libapache2-mod-wsgi-py3
 
@@ -51,11 +51,9 @@ apt install -y libapache2-mod-wsgi-py3
 cat << 'EOF' > app.py
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
 def index():
-    return "Hello, World!"
-
+    return "Hello from Flask!"
 if __name__ == "__main__":
     app.run()
 EOF
@@ -66,34 +64,27 @@ EOF
 cat << 'EOF' > wsgi.py
 #!/usr/bin/python
 import sys
-sys.path.insert(0,"/var/www/mysite/")
-from flasky.app import app as application
+sys.path.insert(0,"/var/www/")
+from mysite.app import app as application
 EOF
 
 # ==========================
 # Let's configure APACHE2
 # ==========================
 cat << 'EOF' > /etc/apache2/sites-enabled/000-default.conf
+ServerName mysite.local
+WSGIPythonPath /var/www/mysite
 <VirtualHost *:80>
-    ErrorLog /var/www/mysite/flasky/logs/error.log
-    CustomLog /var/www/mysite/flasky/logs/access.log combined
-    WSGIDaemonProcess flasky user=www-data group=www-data threads=5
-    WSGIProcessGroup flasky
-    WSGIScriptAlias / /var/www/mysite/flasky/wsgi.py
-    Alias /static/ /var/www/mysite/flasky/static
+    WSGIScriptAlias / /var/www/mysite/wsgi.py
     <Directory /var/www/mysite/mysite>
-        Order allow,deny
-        Allow from all
-        Require all granted
+        <Files wsgi.py>
+            Require all granted
+        </Files>
     </Directory>
 </VirtualHost>
 EOF
 
-# move to mysite directory & change ownership
-cd ../../
-
-chown -R www-data:www-data mysite
-
+chown -R www-data:www-data .
 
 service apache2 start
 
@@ -101,15 +92,24 @@ service apache2 start
 # Now 127.0.0.1 has the user page
 read -p 'Flask is running'
 
-# ==========================================
-# Let's use templates to return an HTML page
-# ==========================================
-cd mysite/flasky
+# ===================
+# Let's use templates
+# ===================
 
 mkdir templates
 
 cat << 'EOF' > templates/index.html
-<h1>Hello Flasky</h1>
+<h1>Hello World</h1>
+EOF
+
+cat << 'EOF' > app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run()
 EOF
 
 service apache2 restart
@@ -143,24 +143,7 @@ cat << 'EOF' > templates/index.html
 {% endblock %}
 EOF
 
-# =====================================
-# Let's modify app.py to return template
-# =====================================
-cat << 'EOF' > app.py
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run()
-EOF
-
-
 service apache2 restart
 
 # Yay, we have a working base template!
 read -p 'The base template is live! Hit enter to continue.'
-
